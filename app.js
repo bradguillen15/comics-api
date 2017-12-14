0b74af7c2c0e978a19c7a1664bd64877e736fff5
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2017-12-14 02:52:51 -0600

    asdf

--- app.js
+++ app.js
@@ -20,6 +20,27 @@ const app = () => {
   expressApp.use(bodyParser.urlencoded({ extended: true }));
   expressApp.use(bodyParser.json());
 
+  app.use(function (req, res) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+   
+});
+
+
+
 
 expressApp.set('views', path.join(__dirname, 'views'));
 expressApp.set('view engine', 'jade');
