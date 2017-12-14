05591b1746f535b097514404ba9227250958e064
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2017-12-14 03:06:23 -0600

    asdfdd

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = () => {
   const expressApp = express();
   expressApp.use(bodyParser.urlencoded({ extended: true }));
   expressApp.use(bodyParser.json());
-expressApp.use(cors({origin: 'null'}));
+expressApp.use(cors({origin: 'http://localhost:8888'}));
 
 
 
