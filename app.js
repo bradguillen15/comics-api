e4d85eea56e6b26a44829f89d0999dec79c2f2dc
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2017-12-14 02:55:53 -0600

    asdf

--- app.js
+++ app.js
@@ -20,24 +20,7 @@ const app = () => {
   expressApp.use(bodyParser.urlencoded({ extended: true }));
   expressApp.use(bodyParser.json());
 
-  expressApp.use(function (req, res) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-   
-});
 
 
 
@@ -95,7 +78,10 @@ console.log(req.body);
   });
 
 
-  expressApp.get('/consola', function(req, res) {
+  expressApp.get('/consola', function(req, res) {    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
   res.render('index', { title: 'Busqueda de Imagenes' });
   });
 
